889a1a330f22f0a55c665f8888ea5835c3c9d341
Mitchell Skaggs <skaggsm333@gmail.com>
2016-03-22 18:03:56 -0500

    Many deprecations, moved from Strings to specific InnovationKeys

--- src/com/javaneat/evolution/NEATGenomeManager.java
+++ src/com/javaneat/evolution/NEATGenomeManager.java
@@ -1,30 +1,50 @@
 package com.javaneat.evolution;
 
+import com.javaneat.genome.InnovationKey;
 import com.javaneat.genome.NEATInnovation;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class NEATGenomeManager {
+    @Deprecated
     private final double disjointGeneCoefficient;
+    @Deprecated
     private final double excessGeneCoefficient;
-    private final Map<String, NEATInnovation> innovations = new HashMap<>();
+    @Deprecated
     private final int numInputs;
+    @Deprecated
     private final int numOutputs;
+    @Deprecated
     private final int populationSize;
+    @Deprecated
     private final double speciesCutoffDelta;
+    @Deprecated
     private final int speciesTarget;
+    @Deprecated
     private final double weightDifferenceCoefficient;
+    @Deprecated
     private final int speciesStagnantTimeLimit;
+    @Deprecated
     private final double mutationWeightProb;
+    @Deprecated
     private final double mutationAddLinkProb;
+    @Deprecated
     private final double mutationAddNodeProb;
+    @Deprecated
     private final double mutationWeightRange;
+    @Deprecated
     private final double mutationWeightWholeProb;
+    @Deprecated
     private final double enableMutationProb;
+    @Deprecated
     private final double disableMutationProb;
+    @Deprecated
     private final double crossoverChance;
+    @Deprecated
     private final double mutationRemoveLinkProb;
+
+    private final Map<InnovationKey, NEATInnovation> innovations = new HashMap<>();
     private int globalInnovationID = 0;
     private int globalNeuronID = 0;
     private double speciesCutoff;
@@ -85,57 +105,66 @@ public class NEATGenomeManager {
             this.acquireNodeInnovation(this.getNewNeuronID()); // Outputs
     }
 
-    public static String getNodeKey(final int nodeID) {
-        return "node_" + nodeID;
+    private static InnovationKey getNodeKey(final int nodeID) {
+        return new InnovationKey(InnovationKey.InnovationType.NODE, nodeID, -1);//"node_" + nodeID;
     }
 
-    public static String getLinkKey(final int fromNode, final int toNode) {
-        return "link_" + fromNode + ":" + toNode;
+    private static InnovationKey getLinkKey(final int fromNode, final int toNode) {
+        return new InnovationKey(InnovationKey.InnovationType.LINK, fromNode, toNode);//"link_" + fromNode + ":" + toNode;
     }
 
-    public static String getSplitKey(final int fromNode, final int toNode) {
-        return "split_" + fromNode + ":" + toNode;
+    private static InnovationKey getSplitKey(final int fromNode, final int toNode) {
+        return new InnovationKey(InnovationKey.InnovationType.SPLIT, fromNode, toNode);//"split_" + fromNode + ":" + toNode;
     }
 
     public NEATInnovation acquireNodeInnovation(final int nodeID) // ONLY for input, output, and bias nodes.
     {
-        final String key = NEATGenomeManager.getNodeKey(nodeID);
+        InnovationKey key = NEATGenomeManager.getNodeKey(nodeID);
         if (!this.innovations.containsKey(key))
             this.innovations.put(key, new NEATInnovation(this.getNewInnovationID(), nodeID));
         return this.innovations.get(key);
     }
 
     public int getNewNeuronID() {
-        return this.globalNeuronID++;
+        int result = this.globalNeuronID;
+        this.globalNeuronID++;
+        return result;
     }
 
-    public int getNewInnovationID() {
-        return this.globalInnovationID++;
+    private int getNewInnovationID() {
+        int result = this.globalInnovationID;
+        this.globalInnovationID++;
+        return result;
     }
 
-    public double getCrossoverChance() {
+    @Deprecated
+    private double getCrossoverChance() {
         return this.crossoverChance;
     }
 
-    public int getSpeciesStagnantTimeLimit() {
+    @Deprecated
+    private int getSpeciesStagnantTimeLimit() {
         return this.speciesStagnantTimeLimit;
     }
 
-    public double getEnableMutationProb() {
+    @Deprecated
+    private double getEnableMutationProb() {
         return this.enableMutationProb;
     }
 
-    public double getDisableMutationProb() {
+    @Deprecated
+    private double getDisableMutationProb() {
         return this.disableMutationProb;
     }
 
-    public double getMutationWeightWholeProb() {
+    @Deprecated
+    private double getMutationWeightWholeProb() {
         return this.mutationWeightWholeProb;
     }
 
     public NEATInnovation acquireLinkInnovation(final int fromNode, final int toNode) // For a link mutation
     {
-        final String key = NEATGenomeManager.getLinkKey(fromNode, toNode);
+        InnovationKey key = NEATGenomeManager.getLinkKey(fromNode, toNode);
         if (!this.innovations.containsKey(key))
             this.innovations.put(key, new NEATInnovation(this.getNewInnovationID(), -1));
         return this.innovations.get(key);
@@ -143,7 +172,7 @@ public class NEATGenomeManager {
 
     public NEATInnovation acquireSplitInnovation(final int fromNode, final int toNode) // For a split mutation
     {
-        final String key = NEATGenomeManager.getSplitKey(fromNode, toNode);
+        InnovationKey key = NEATGenomeManager.getSplitKey(fromNode, toNode);
         if (!this.innovations.containsKey(key))
             this.innovations.put(key, new NEATInnovation(this.getNewInnovationID(), this.getNewNeuronID()));
         return this.innovations.get(key);
@@ -153,38 +182,47 @@ public class NEATGenomeManager {
         return 0;
     }
 
+    @Deprecated
     public double getDisjointGeneCoefficient() {
         return this.disjointGeneCoefficient;
     }
 
+    @Deprecated
     public double getExcessGeneCoefficient() {
         return this.excessGeneCoefficient;
     }
 
+    @Deprecated
     public int getHiddenOffset() {
         return 1 + this.getNumInputs() + this.getNumOutputs();
     }
 
+    @Deprecated
     public int getNumInputs() {
         return this.numInputs;
     }
 
+    @Deprecated
     public int getNumOutputs() {
         return this.numOutputs;
     }
 
+    @Deprecated
     public int getInputOffset() {
         return 1;
     }
 
+    @Deprecated
     public int getOutputOffset() {
         return 1 + this.getNumInputs();
     }
 
-    public int getPopulationSize() {
+    @Deprecated
+    int getPopulationSize() {
         return this.populationSize;
     }
 
+    @Deprecated
     public double getSpeciesCutoff() {
         return this.speciesCutoff;
     }
@@ -192,19 +230,23 @@ public class NEATGenomeManager {
     /**
      * @return the speciesCutoffDelta
      */
+    @Deprecated
     public double getSpeciesCutoffDelta() {
         return this.speciesCutoffDelta;
     }
 
-    public int getSpeciesTarget() {
+    @Deprecated
+    int getSpeciesTarget() {
         return this.speciesTarget;
     }
 
+    @Deprecated
     public double getWeightDifferenceCoefficient() {
         return this.weightDifferenceCoefficient;
     }
 
-    public void tweakSpeciesCutoff(final boolean up) {
+    @Deprecated
+    void tweakSpeciesCutoff(final boolean up) {
         if (this.speciesCutoff + (up ? this.speciesCutoffDelta : -this.speciesCutoffDelta) > 0)
             this.speciesCutoff += up ? this.speciesCutoffDelta : -this.speciesCutoffDelta;
     }
@@ -212,35 +254,40 @@ public class NEATGenomeManager {
     /**
      * @return the mutationWeightProb
      */
-    public double getMutationWeightProb() {
+    @Deprecated
+    double getMutationWeightProb() {
         return mutationWeightProb;
     }
 
     /**
      * @return the mutationAddLinkProb
      */
-    public double getMutationAddLinkProb() {
+    @Deprecated
+    double getMutationAddLinkProb() {
         return mutationAddLinkProb;
     }
 
     /**
      * @return the mutationAddNodeProb
      */
-    public double getMutationAddNodeProb() {
+    @Deprecated
+    double getMutationAddNodeProb() {
         return mutationAddNodeProb;
     }
 
     /**
      * @return the mutationWeightRange
      */
-    public double getMutationWeightRange() {
+    @Deprecated
+    double getMutationWeightRange() {
         return mutationWeightRange;
     }
 
     /**
      * @return the mutationRemoveLinkProb
      */
-    public double getMutationRemoveLinkProb() {
+    @Deprecated
+    double getMutationRemoveLinkProb() {
         return mutationRemoveLinkProb;
     }
 }
--- src/com/javaneat/genome/InnovationKey.java
+++ src/com/javaneat/genome/InnovationKey.java
@@ -1,11 +1,41 @@
 package com.javaneat.genome;
 
-enum InnovationType {
-    NODE, LINK, SPLIT
-}
-
 /**
  * Created by Mitchell on 1/18/2016.
  */
 public class InnovationKey {
+    private final InnovationType type;
+    private final int fromNode;
+    private final int toNode;
+
+    public InnovationKey(InnovationType type, int fromNode, int toNode) {
+        this.type = type;
+        this.fromNode = fromNode;
+        this.toNode = toNode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        InnovationKey that = (InnovationKey) o;
+
+        if (fromNode != that.fromNode) return false;
+        if (toNode != that.toNode) return false;
+        return type == that.type;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = type.hashCode();
+        result = 31 * result + fromNode;
+        result = 31 * result + toNode;
+        return result;
+    }
+
+    public enum InnovationType {
+        NODE, LINK, SPLIT
+    }
 }
