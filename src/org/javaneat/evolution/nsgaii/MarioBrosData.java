2f7e62ccb85ea45dfeede1a6e5c3572c2674ebda
Mitchell Skaggs <skaggsm333@gmail.com>
2016-04-07 05:46:28 -0500

    Added hashcode and equals methods

--- src/org/javaneat/evolution/nsgaii/MarioBrosData.java
+++ src/org/javaneat/evolution/nsgaii/MarioBrosData.java
@@ -10,8 +10,25 @@ import java.util.List;
 public class MarioBrosData implements Serializable {
     public final List<DataPoint> dataPoints;
 
+
     public MarioBrosData() {
         dataPoints = new LinkedList<>();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MarioBrosData that = (MarioBrosData) o;
+
+        return dataPoints.equals(that.dataPoints);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return dataPoints.hashCode();
     }
 
     @Override
@@ -45,6 +62,38 @@ public class MarioBrosData implements Serializable {
         @Override
         public String toString() {
             return String.format("%d %d %d %d %d %d %d %d", score, time, world, level, lives, marioX, marioY, marioState);
+        }
+
+        @SuppressWarnings("SimplifiableIfStatement")
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            DataPoint dataPoint = (DataPoint) o;
+
+            if (score != dataPoint.score) return false;
+            if (time != dataPoint.time) return false;
+            if (world != dataPoint.world) return false;
+            if (level != dataPoint.level) return false;
+            if (lives != dataPoint.lives) return false;
+            if (marioX != dataPoint.marioX) return false;
+            if (marioY != dataPoint.marioY) return false;
+            return marioState == dataPoint.marioState;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = score;
+            result = 31 * result + time;
+            result = 31 * result + world;
+            result = 31 * result + level;
+            result = 31 * result + lives;
+            result = 31 * result + marioX;
+            result = 31 * result + marioY;
+            result = 31 * result + marioState;
+            return result;
         }
     }
 }
